99c06d993ce687b83029f72c631972eb89d8db6b
Will <wsamuels@asu.edu>
2017-04-10 23:46:28 -0500

    Made newAccount return a boolean value

--- src/banking/primitive/core/ServerSolution.java
+++ src/banking/primitive/core/ServerSolution.java
@@ -71,7 +71,7 @@ class ServerSolution implements AccountServer {
 	public boolean newAccount(String type, String name, float balance) 
 		throws IllegalArgumentException {
 		
-		if (balance < 0.0f) throw new IllegalArgumentException("New account may not be started with a negative balance");
+		if (balance < 0.0f) {return false;}//throw new IllegalArgumentException("New account may not be started with a negative balance");
 		
 		return newAccountFactory(type, name, balance);
 	}
